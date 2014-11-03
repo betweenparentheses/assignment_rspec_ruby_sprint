afd7e1267096ce223af72e4769cd167ea000dde9
Michael Alexander <alexandermw@gmail.com>
2014-11-03 14:54:36 -0800

    subtract and multiply

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -17,21 +17,46 @@ describe Calculator do
       expect(c.add(1.5, 2.5)).to eq(4.0)
     end
     
-    it 'can add a float to an integer' do
+    it 'returns a float when adding a float to an integer' do
       expect(c.add(1, 0.3)).to eq(1.3)
     end
     
   end
   
-  
+  # The subtract method properly subtracts numbers, whether positive, negative, integer or float.
   describe '#subtract' do
     
+    it 'returns the difference of its arguments' do
+      expect(c.subtract(4, 2)).to eq(2)
+    end
     
+    it 'correctly cancels negatives' do
+      expect(c.subtract(4, -2)).to eq(6)
+    end
     
+    it 'subtracts two floats' do
+      expect(c.subtract(2.5, 0.3)).to eq(2.2)
+    end
+    
+    it 'returns a float when subtracting a float from an integer' do
+      expect(c.subtract(5, 2.5)).to eq(2.5)
+    end
     
   end
   
   describe '#multiply' do
+    
+    it 'returns the product of its arguments' do
+      expect(c.multiply(6, 8)).to eq(48)
+    end
+    
+    it 'returns a negative number when multiplying a positive times a negative' do
+      expect(c.multiply(6, -8)).to eq(-48)
+    end
+    
+    it 'multiplies with floats' do
+      expect(c.multiply(2.0 * 3.3)).to eq(6.6)
+    end
     
   end
   
